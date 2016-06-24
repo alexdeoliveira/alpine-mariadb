fa38f554299113b414050705ae9764538d31e395
Aléx de Oliveira <alex.oliveira.web@gmail.com>
2016-06-24 11:26:21 -0300

    Change user

--- mariadb_init.sh
+++ mariadb_init.sh
@@ -4,17 +4,16 @@
 
 mysqladmin --silent --wait=30 ping
 
-mysql -uroot -e "CREATE USER 'homestead'@'%' IDENTIFIED BY 'secret'"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'homestead'@'%' WITH GRANT OPTION"
+mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY 'secret'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
 mysql -uroot -e "FLUSH PRIVILEGES"
-mysql -uroot -e "CREATE SCHEMA homestead"
 
 echo "=> Done!"
 
 echo "========================================================================"
 echo "You can now connect to this MariaDB Server using:"
 echo ""
-echo "    mysql -uhomestead -psecret -h<host> -P<port>"
+echo "    mysql -uadmin -psecret -h<host> -P<port>"
 echo ""
 echo "Please remember to change the above password as soon as possible!"
 echo "MariaDB user 'root' has no password but only allows local connections"
